e9e1eb742d41172cad8595e15dbccf0e64a0011b
Igor the roofCoder <53712180+russian-droid@users.noreply.github.com>
2021-02-18 22:42:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#from codewars.com
+#CodeWars kata codewars.com
 #alphabet_position("The sunset sets at twelve o' clock.")
 #Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
 
